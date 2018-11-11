4c7429c285fba714c9ddabcb48632ed39b41d394
Mryang <905032390@qq.com>
2018-11-11 20:16:19 +0800

    商品条件查询前端代码组装数据完成,品牌过滤条件查询功能实现,规格过滤条件查询功能实现,价格区间过滤条件查询功能实现,去除选择的条件功能实现。

--- pinyougou_parent/pinyougou_search_service/src/main/java/com/pinyougou/search/service/impl/SearchServiceImpl.java
+++ pinyougou_parent/pinyougou_search_service/src/main/java/com/pinyougou/search/service/impl/SearchServiceImpl.java
@@ -5,16 +5,14 @@ import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.HighlightOptions;
-import org.springframework.data.solr.core.query.HighlightQuery;
-import org.springframework.data.solr.core.query.SimpleHighlightQuery;
+import org.springframework.data.solr.core.query.*;
 import org.springframework.data.solr.core.query.result.HighlightEntry;
 import org.springframework.data.solr.core.query.result.HighlightPage;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -25,15 +23,56 @@ public class SearchServiceImpl implements SearchService {
     @Override
     public Map<String, Object> search(Map searchMap) {
         HighlightQuery query = new SimpleHighlightQuery();
+        //输入条件查询
         String keywords = (String) searchMap.get("keywords");
         Criteria criteria = null;
-        if (keywords != null && !"".equals(keywords)){
+        if (keywords != null && !"".equals(keywords)) {
             //输入了关键字
             criteria = new Criteria("item_keywords").is(keywords);
-        }else{
+        } else {
             criteria = new Criteria().expression("*:*");
         }
+        //条件品牌查询
+        String brand = (String) searchMap.get("brand");
+        if (brand != null && !"".equals(brand)) {
+            Criteria brandCriteria = new Criteria("item_brand").is(brand);
+            SimpleFilterQuery filterQuery = new SimpleFilterQuery(brandCriteria);
+            query.addFilterQuery(filterQuery);
+        }
+        //条件分类查询
+        String category = (String) searchMap.get("category");
+        if (category != null && !"".equals(category)) {
+            Criteria categoryCriteria = new Criteria("item_category").is(category);
+            SimpleFilterQuery filterQuery = new SimpleFilterQuery(categoryCriteria);
+            query.addFilterQuery(filterQuery);
+        }
 
+        //条件规格查询
+        Map<String, String> specMap = (Map<String, String>) searchMap.get("spec");
+        if (specMap != null) {
+            for (String key : specMap.keySet()) {
+                Criteria specCriteria = new Criteria("item_spec_"+key).is(specMap.get(key));
+                SimpleFilterQuery filterQuery = new SimpleFilterQuery(specCriteria);
+                query.addFilterQuery(filterQuery);
+            }
+        }
+
+        //条件价格查询
+        String price = (String) searchMap.get("price");
+        if (price != null && !"".equals(price)) {
+            String[] split = price.split(("-"));
+            if (!"0".equals(split[0])){
+                Criteria priceCriteria = new Criteria("item_price").greaterThanEqual(split[0]);
+                SimpleFilterQuery filterQuery = new SimpleFilterQuery(priceCriteria);
+                query.addFilterQuery(filterQuery);
+
+            }
+            if (!"*".equals(split[1])){
+                Criteria priceCriteria = new Criteria("item_price").lessThanEqual(split[1]);
+                SimpleFilterQuery filterQuery = new SimpleFilterQuery(priceCriteria);
+                query.addFilterQuery(filterQuery);
+            }
+        }
         query.addCriteria(criteria);
 
         HighlightOptions highlightOptions = new HighlightOptions();
@@ -46,18 +85,18 @@ public class SearchServiceImpl implements SearchService {
         List<TbItem> content = page.getContent();
 
         for (TbItem item : content) {
-            List<HighlightEntry.Highlight> highlights=page.getHighlights(item);
-            if (highlights.size()>0){
+            List<HighlightEntry.Highlight> highlights = page.getHighlights(item);
+            if (highlights.size() > 0) {
                 HighlightEntry.Highlight highlight = highlights.get(0);
                 List<String> snipplets = highlight.getSnipplets();
-                if (snipplets.size()>0){
+                if (snipplets.size() > 0) {
                     item.setTitle(snipplets.get(0));
                 }
             }
         }
 
-        Map<String,Object> resultMap = new HashMap<>();
-        resultMap.put("rows",content);
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("rows", content);
         return resultMap;
     }
 }
--- pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
+++ pinyougou_parent/pinyougou_search_web/src/main/webapp/js/controller/searchController.js
@@ -4,11 +4,38 @@ app.controller('searchController', function ($scope, $controller, searchService)
     $controller('baseController', {$scope: $scope});//继承
 
     $scope.searchMap = {
-        keywords: ""
+        keywords:"",
+        category:"",
+        brand:"",
+        spec:{},
+        price:"",
+        sortField:"",
+        sort:"ASC",
+        pageNo:1,
+        pageSize:60
     };
     $scope.search = function () {
         searchService.search($scope.searchMap).success(function (response) {
             $scope.resultMap = response;
         })
     };
+
+    //封装条件查询
+    $scope.addFilterCondition = function (key, value) {
+        if (key == "category" || key == "brand" || key == "price") {
+            $scope.searchMap[key] = value;
+        } else {
+            $scope.searchMap.spec[key]=value;
+        }
+        $scope.search();
+    }
+
+    $scope.removeSearchItem = function (key) {
+        if (key == "category" || key == "brand" || key == "price") {
+            $scope.searchMap[key] ="";
+        } else {
+            delete $scope.searchMap.spec[key];
+        }
+        $scope.search();
+    }
 });	

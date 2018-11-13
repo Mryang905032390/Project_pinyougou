9a8f020504c24e89ed2ac2ac5948bca7904e8a80
Mryang <905032390@qq.com>
2018-11-13 23:48:15 +0800

    更新.gitignore

--- pinyougou_parent/pinyougou_shop_web/target/pinyougou_shop_web-1.0-SNAPSHOT/js/controller/indexController.js
+++
@@ -1,9 +0,0 @@
-app.controller("indexController", function ($scope, $controller, loginService) {
-    $controller("baseController", {$scope: $scope});
-    //获取登录名
-    $scope.getName=function () {
-        loginService.getName().success(function (response) {
-            $scope.userName=response.loginName;
-        })
-    }
-});

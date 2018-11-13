2435da59ea6a86f33589a0bcb27ecac0c1fe5b27
Mryang <905032390@qq.com>
2018-11-13 19:56:57 +0800

    商品静态页生成后端代码实现，商品静态页生成基本数据插值替换并测试完成。

---
+++ pinyougou_parent/pinyougou_page_service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
@@ -0,0 +1,45 @@
+package com.pinyougou.page.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.TbGoodsDescMapper;
+import com.pinyougou.mapper.TbGoodsMapper;
+import com.pinyougou.mapper.TbItemMapper;
+import com.pinyougou.page.service.ItemPageService;
+import com.pinyougou.pojo.TbGoods;
+import com.pinyougou.pojo.TbGoodsDesc;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.pojo.TbItemExample;
+import groupEntity.Goods;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ItemPageServiceImpl implements ItemPageService{
+    @Autowired
+    private TbGoodsMapper tbGoodsMapper;
+
+    @Autowired
+    private TbGoodsDescMapper tbGoodsDescMapper;
+
+    @Autowired
+    private TbItemMapper tbItemMapper;
+    @Override
+    public Goods findOne(Long goodsId) {
+        Goods goods = new Goods();
+        TbGoods tbGoods = tbGoodsMapper.selectByPrimaryKey(goodsId);
+        goods.setGoods(tbGoods);
+
+        TbGoodsDesc tbGoodsDesc = tbGoodsDescMapper.selectByPrimaryKey(goodsId);
+        goods.setGoodsDesc(tbGoodsDesc);
+
+        TbItemExample example = new TbItemExample();
+        TbItemExample.Criteria criteria = example.createCriteria();
+        criteria.andGoodsIdEqualTo(goodsId);
+        List<TbItem> itemList = tbItemMapper.selectByExample(example);
+        goods.setItemList(itemList);
+        return goods;
+    }
+}
